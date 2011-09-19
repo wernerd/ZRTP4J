5e303b6049ecea5c8ed99a92ce3e51283caa5a8c
wernerd <Werner.Dittmann@t-online.de>
2011-09-19 19:01:02 +0200

    Set version number to 2.1.0 .

--- src/gnu/java/zrtp/ZrtpConstants.java
+++ src/gnu/java/zrtp/ZrtpConstants.java
@@ -53,7 +53,7 @@ public class ZrtpConstants {
     // The following string must contain ASCII characters only
     //                                               1
     //                                     0123456789012345
-    public static final String clientId = "GNU ZRTP4J 1.6.1"  ;
+    public static final String clientId = "GNU ZRTP4J 2.1.0"  ;
  
 //  "1.10"
     public static final byte[] zrtpVersion = {
